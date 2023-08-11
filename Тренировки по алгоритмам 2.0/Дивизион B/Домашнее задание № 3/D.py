c4876f3762640a4082f71645924e0a77ad8c797c
Igor Md <beerk0ff@yandex.ru>
2023-08-11 21:09:45 +0600

    algs 2.0. Div B. 3D.

---
+++ Тренировки по алгоритмам 2.0/Дивизион B/Домашнее задание № 3/D.py
@@ -0,0 +1,25 @@
+def read_input():
+    n = int(input())
+    return n
+
+
+def meet_num(n):
+    possible_nums = {x for x in range(1, n + 1)}
+    while True:
+        line = input().lower()
+        if line == 'help':
+            return sorted(possible_nums)
+        asked_set = {int(x) for x in line.split()}
+        answer = input().lower()
+        if answer == 'no':
+            possible_nums.difference_update(asked_set)
+        elif answer == 'yes':
+            possible_nums.intersection_update(asked_set)
+
+
+def main():
+    print(*meet_num(read_input()))
+
+
+if __name__ == '__main__':
+    main()
