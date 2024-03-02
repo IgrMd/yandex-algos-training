10efad4d5b1333191b6fea53a21d231799d38deb
Igor Md <beerk0ff@yandex.ru>
2024-03-02 12:43:38 +0500

    algos 5.0. lecture 1. A refact

--- Тренировки по алгоритмам 5.0/Лекция 1. Сложность, тестирование, особые случаи/A.py
+++ Тренировки по алгоритмам 5.0/Лекция 1. Сложность, тестирование, особые случаи/A.py
@@ -5,12 +5,12 @@ def read_input():
 
 
 def paint_tries(p, v, q, m):
-    intrval1 = [p - v, p + v]
-    intrval2 = [q - m, q + m]
-    if (intrval2[0] <= intrval1[0] <= intrval2[1]) or (intrval1[0] <= intrval2[0] <= intrval1[1]):
-        return max(intrval1[1], intrval2[1]) - min(intrval1[0], intrval2[0]) + 1
+    interval1 = [p - v, p + v]
+    interval2 = [q - m, q + m]
+    if (interval2[0] <= interval1[0] <= interval2[1]) or (interval1[0] <= interval2[0] <= interval1[1]):
+        return max(interval1[1], interval2[1]) - min(interval1[0], interval2[0]) + 1
     else:
-        return intrval1[1] - intrval1[0] + intrval2[1] - intrval2[0] + 2
+        return interval1[1] - interval1[0] + interval2[1] - interval2[0] + 2
 
 
 def main():
