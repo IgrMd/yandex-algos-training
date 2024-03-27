0b5ba1e3b26d99fa2e66c2fd401df9018f50bb92
Igor Md <beerk0ff@yandex.ru>
2024-03-27 19:03:15 +0500

    algos 5.0. lecture 3. I

--- Тренировки по алгоритмам 5.0/Лекция 4. Бинарный поиск/I.py
+++ Тренировки по алгоритмам 5.0/Лекция 4. Бинарный поиск/I.py
@@ -71,36 +71,17 @@ def is_good_point(point: Point, d):
     return point.y >= 0 and (point.x * point.x + point.y * point.y <= d * d)
 
 
-def upper_bound(l, r, cmp, params):
-    while l < r:
-        mid = (l + r) // 2
-        if cmp(mid, *params):
-            r = mid
-        else:
-            l = mid + 1
-    if not cmp(l, *params):
-        l += 1
-    return l
-
-
 def lower_bound_f(l, r, eps, cmp, params):
+    mem_p = Point(0, 0)
     while abs(r - l) >= eps:
         mid = (l + r) / 2
-        if cmp(mid, *params):
+        check, point_ = cmp(mid, *params)
+        if check:
+            mem_p = point_
             l = mid
         else:
             r = mid
-    # if not cmp(l, *params):
-    #     l -= EPS
-    return l
-
-
-def circles_intersect_line(c: Circle, y: int):
-    if c.p.y + c.r <= y or y <= c.p.y - c.r:
-        return None
-    dy = y - c.p.y
-    dx = (c.r * c.r - dy * dy) ** 0.5
-    return [Point(c.p.x + dx, y), Point(c.p.x - dx, y)]
+    return l, mem_p
 
 
 def check_time(t, d, n, players: list[Player]):
@@ -109,42 +90,45 @@ def check_time(t, d, n, players: list[Player]):
     for player in players:
         circles.append(Circle(player.p.x, player.p.y, player.v * t))
         if circles[-1].is_p_inside(p1) and circles[-1].is_p_inside(p2) and circles[-1].is_p_inside(p3):
-            return False
+            return False, None
     circles.append(Circle(0, 0, d))
-    points = []
-    for i in range(n):
-        for j in range(i + 1, n + 1):
-            c1 = circles[i]
-            c2 = circles[j]
-            buf = find_intersection(c1, c2)
-            for point in buf:
-                if j == n:
-                    if point.y >= 0:
-                        points.append((point, i, j))
-                elif is_good_point(point, d):
-                    points.append((point, i, j))
+    points = intersect_circles(circles, d, n)
     if not len(points):
         all_inside = True
         all_outside = True
         for i in range(n):
             c = circles[i]
             dist = (c.p.x * c.p.x + c.p.y * c.p.y) ** 0.5
-            all_inside = dist + c.r < d
-            all_outside = dist > d + c.r
-        if all_inside:
-            return True
-        if all_outside:
-            return True
-        return False
-    for point, i, j in points:
+            all_inside = all_inside or dist + c.r < d
+            all_outside = all_outside or dist > d + c.r
+        if all_inside or all_outside:
+            return True, Point(0, 0)
+        return False, None
+    for point_, i, j in points:
         flag = False
         for k in range(n):
             if k == i or k == j:
                 continue
-            flag = flag or circles[k].is_p_inside(point)
+            flag = flag or circles[k].is_p_inside(point_)
         if not flag:
-            return True
-    return False
+            return True, point_
+    return False, None
+
+
+def intersect_circles(circles, d, n):
+    points = []
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            c1 = circles[i]
+            c2 = circles[j]
+            buf = find_intersection(c1, c2)
+            for p in buf:
+                if j == n:
+                    if p.y >= 0:
+                        points.append((p, i, j))
+                elif is_good_point(p, d):
+                    points.append((p, i, j))
+    return points
 
 
 def find_max_time(d, players: list[Player]):
@@ -160,42 +144,9 @@ EPS = 1e-9
 
 def strike(d, n, players: list[Player]):
     max_time = find_max_time(d, players)
-    t = lower_bound_f(0, max_time, EPS, check_time, (d, n, players))
+    t, p = lower_bound_f(0, max_time, EPS, check_time, (d, n, players))
+    return t, p
 
-    circles = []
-    for player in players:
-        circles.append(Circle(player.p.x, player.p.y, player.v * t))
-    circles.append(Circle(0, 0, d))
-    points = []
-    for i in range(n):
-        for j in range(i + 1, n + 1):
-            c1 = circles[i]
-            c2 = circles[j]
-            buf = find_intersection(c1, c2)
-            for point in buf:
-                if j == n:
-                    if point.y >= 0:
-                        points.append((point, i, j))
-                elif is_good_point(point, d):
-                    points.append((point, i, j))
-    for i in range(n):
-        if buf := circles_intersect_line(circles[i], 0):
-            points.extend(buf)
-    for point, i, j in points:
-        flag = False
-        for k in range(n):
-            if k == i or k == j:
-                continue
-            flag = flag or circles[k].is_p_inside(point)
-        if not flag:
-            break
-
-    return t, point
-
-
-# find_intersection(Circle(1, 1, 3), Circle(3, 1, 2))
-# find_intersection(Circle(1, 1, 1), Circle(2, 2, 1))
-# find_intersection(Circle(1, 1, 1), Circle(1, 3, 1))
 
 if __name__ == '__main__':
     t, point = strike(*read_input())
