7618ba8a373467e4ea271d91380c8d303b46ec94
Igor Md <beerk0ff@yandex.ru>
2024-03-28 20:50:15 +0500

    algos 5.0. lecture 4. A

--- Тренировки по алгоритмам 5.0/Лекция 4. Бинарный поиск/A.py
+++ Тренировки по алгоритмам 5.0/Лекция 4. Бинарный поиск/A.py
@@ -1,3 +1,6 @@
+from bisect import bisect_left, bisect_right
+
+
 def read_input():
     n = int(input())
     arr = list(map(int, input().split()))
@@ -8,45 +11,13 @@ def read_input():
     return n, arr, k, requests
 
 
-def cmp_gt(i, target, arr):
-    return arr[i] > target
-
-
-def cmp_le(i, target, arr):
-    return arr[i] >= target
-
-
-def upper_bound(l, r, cmp, params):
-    r -= 1
-    while l < r:
-        mid = (l + r) // 2
-        if cmp(mid, *params):
-            r = mid
-        else:
-            l = mid + 1
-    if not cmp(l, *params):
-        return l + 1
-    return l
-
-
-def lower_bound(l, r, cmp, params):
-    r -= 1
-    while l < r:
-        mid = (l + r) // 2
-        if cmp(mid, *params):
-            r = mid
-        else:
-            l = mid + 1
-    if not cmp(l, *params):
-        return l + 1
-    return l
-
-
 def execute(n, arr, k, requests):
+    result = []
     for L, R in requests:
-        ans = upper_bound(0, n, cmp_gt, (R, arr)) - upper_bound(0, n, cmp_le, (L, arr))
-        print(ans, end=' ')
+        ans = bisect_right(arr, R) - bisect_left(arr, L)
+        result.append(ans)
+    return result
 
 
 if __name__ == '__main__':
-    execute(*read_input())
+    print(*execute(*read_input()))
