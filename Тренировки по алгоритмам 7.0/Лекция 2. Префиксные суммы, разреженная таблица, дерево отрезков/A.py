13d056c132d1b9eae38b44d751a7a90539f4415e
Igor Md <beerk0ff@yandex.ru>
2025-04-17 17:08:41 +0300

    algos 7.0. 2 A

--- Тренировки по алгоритмам 7.0/Лекция 2. Префиксные суммы, разреженная таблица, дерево отрезков/A.py
+++ Тренировки по алгоритмам 7.0/Лекция 2. Префиксные суммы, разреженная таблица, дерево отрезков/A.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 
 def read_input():
     n = int(input().strip())
-    tree = SegmentTree(list(map(int, input().split())))
-    return tree
+    table = SegmentTree(list(map(int, input().split())))
+    return table
 
 
 @dataclass
@@ -16,7 +16,7 @@ class Range:
 @dataclass
 class Item:
     max: int | float
-    max_count: int
+    count: int
     rng: Range
 
 
@@ -34,23 +34,24 @@ class SegmentTree:
             k += 1
         pow_of_2: int = 2 ** k
         self.n = 2 * pow_of_2 - 1
-        self.initial_i = pow_of_2 - 1
+        self.displacement = pow_of_2 - 1
         self.arr: list[Item | None] = [None] * self.n
-        for i in range(len(arr)):
-            self_index: int = i + self.initial_i
-            self.arr[self_index] = Item(arr[i], 1, Range(i, i))
-        for i in range(self.initial_i + len(arr), self.n):
-            self.arr[i] = Item(-INF, 1, Range(i - self.initial_i, i - self.initial_i))
-        for i in range(self.initial_i - 1, -1, -1):
-            l_child_i, r_child_i = self._get_children_(i)
-            l_child, r_child = self.arr[l_child_i], self.arr[r_child_i]
-            cur_max = max(l_child.max, r_child.max)
-            rng = Range(l_child.rng.l, r_child.rng.r)
-            max_count = 0
-            for child in (l_child_i, r_child_i):
-                if cur_max == self.arr[child].max:
-                    max_count += self.arr[child].max_count
-            self.arr[i] = Item(cur_max, max_count, rng)
+        for i in range(self.n - 1, -1, -1):
+            initial_i = i - self.displacement
+            if initial_i >= len(arr):
+                self.arr[i] = Item(-INF, 0, Range(initial_i, initial_i))
+            elif initial_i >= 0:
+                self.arr[i] = Item(arr[initial_i], 1, Range(initial_i, initial_i))
+            else:
+                l_child_i, r_child_i = self._get_children_(i)
+                l_child, r_child = self.arr[l_child_i], self.arr[r_child_i]
+                cur_max = max(l_child.max, r_child.max)
+                rng = Range(l_child.rng.l, r_child.rng.r)
+                cur_count = 0
+                for child in (l_child, r_child):
+                    if child.max == cur_max:
+                        cur_count += child.count
+                self.arr[i] = Item(cur_max, cur_count, rng)
 
     @staticmethod
     def _get_children_(i: int):
@@ -63,17 +64,16 @@ class SegmentTree:
         if request.r < me.rng.l or request.l > me.rng.r:
             return -INF, 0
         if request.l <= me.rng.l and me.rng.r <= request.r:
-            return me.max, me.max_count
-        child_l, child_r = self._get_children_(i)
-        max_l, max_count_l = self._handle_request_impl_(child_l, request)
-        max_r, max_count_r = self._handle_request_impl_(child_r, request)
-        max_ans = max(max_l, max_r)
-        count_ans = 0
-        if max_ans == max_l:
-            count_ans += max_count_l
-        if max_ans == max_r:
-            count_ans += max_count_r
-        return max_ans, count_ans
+            return me.max, me.count
+        l_child_i, r_child_i = self._get_children_(i)
+        l_ans = self._handle_request_impl_(l_child_i, request)
+        r_ans = self._handle_request_impl_(r_child_i, request)
+        cur_max = max(l_ans[0], r_ans[0])
+        cur_count = 0
+        for ans in (l_ans, r_ans):
+            if ans[0] == cur_max:
+                cur_count += ans[1]
+        return cur_max, cur_count
 
     def handle_request(self, rng: Range):
         return self._handle_request_impl_(0, rng)
