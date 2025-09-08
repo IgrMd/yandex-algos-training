8e00a35e185e27e53615ed7f6688f78e8347d2f2
Igor Md <beerk0ff@yandex.ru>
2025-09-08 20:58:30 +0300

    algos 1.0 5 A

--- Тренировки по алгоритмам 1.0/Лекция 5. «Префиксные суммы и два указателя»/A.py
+++ Тренировки по алгоритмам 1.0/Лекция 5. «Префиксные суммы и два указателя»/A.py
@@ -7,36 +7,19 @@ def read_input():
 
 
 def style(n, m, shirts, pants):
-    l = 0
-    r = 0
-    wardrobe = []
-    while l < n and r < m:
-        if shirts[l] < pants[r]:
-            wardrobe.append((shirts[l], 0))
-            l += 1
+    i = 0
+    j = 0
+    min_diff = float('inf')
+    i_min, j_min = 0, 0
+    while i < n and j < m:
+        if abs(shirts[i] - pants[j]) < min_diff:
+            min_diff = abs(shirts[i] - pants[j])
+            i_min, j_min = i, j
+        if shirts[i] < pants[j]:
+            i += 1
         else:
-            wardrobe.append((pants[r], 1))
-            r += 1
-
-    while l < n:
-        wardrobe.append((shirts[l], 0))
-        l += 1
-    while r < m:
-        wardrobe.append((pants[r], 1))
-        r += 1
-
-    max_style = wardrobe[-1][0] - wardrobe[1][0]
-    pair = 0
-    for i in range(len(wardrobe) - 1):
-        if wardrobe[i][1] == wardrobe[i + 1][1]:
-            continue
-        if wardrobe[i + 1][0] - wardrobe[i][0] < max_style:
-            max_style = wardrobe[i + 1][0] - wardrobe[i][0]
-            pair = i
-    if wardrobe[pair][1] == 0:
-        return wardrobe[pair][0], wardrobe[pair + 1][0]
-    else:
-        return wardrobe[pair + 1][0], wardrobe[pair][0]
+            j += 1
+    return shirts[i_min], pants[j_min]
 
 
 def main():
