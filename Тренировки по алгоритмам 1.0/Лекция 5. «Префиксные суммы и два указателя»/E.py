3eb59e3ed4f56d62c22d2d3c489a0c3114e375de
Igor Md <beerk0ff@yandex.ru>
2025-09-10 18:02:39 +0300

    algos 1.0 5 E

--- Тренировки по алгоритмам 1.0/Лекция 5. «Префиксные суммы и два указателя»/E.py
+++ Тренировки по алгоритмам 1.0/Лекция 5. «Префиксные суммы и два указателя»/E.py
@@ -9,27 +9,18 @@ def read_input():
 
 def coords(n, k, trees):
     trees_met = defaultdict(int)
-    inf = 10 ** 6
-    prev = [inf] * (n + 1)
-    l = r = 0
-    while r < n:
-        while r < n and len(trees_met) < k:
-            trees_met[trees[r]] += 1
-            r += 1
-        while l < r and len(trees_met) == k:
+    l_ans, r_ans = 0, n - 1
+    l = 0
+    for r in range(n):
+        trees_met[trees[r]] += 1
+        while len(trees_met) == k:
+            if r - l < r_ans - l_ans:
+                r_ans, l_ans = r, l
             trees_met[trees[l]] -= 1
             if trees_met[trees[l]] == 0:
                 trees_met.pop(trees[l])
             l += 1
-            prev[r] = l
-
-    min_interval = inf
-    answer = (1, n)
-    for i in range(1, n + 1):
-        if prev[i] != inf and i - prev[i] < min_interval:
-            min_interval = i - prev[i]
-            answer = (prev[i], i)
-    return answer
+    return l_ans + 1, r_ans + 1
 
 
 def main():
