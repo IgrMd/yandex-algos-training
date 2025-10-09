a133b8fa16e3928f864a1283b934bea57eb133d5
Igor Md <beerk0ff@yandex.ru>
2025-10-09 20:40:08 +0300

    algos 8.0 3,4 H

--- Тренировки по алгоритмам 8.0/Тема 3, 4. Одномерное динамическое программирование. Двумерное динамическое программирование/H.py
+++ Тренировки по алгоритмам 8.0/Тема 3, 4. Одномерное динамическое программирование. Двумерное динамическое программирование/H.py
@@ -8,7 +8,9 @@ HIM = 1
 
 
 def matches(n: int):
-    count = max(5, n + 1)
+    if n < 4:
+        return 1
+    count = n + 1
     dp = [['B', 'B'] for _ in range(count)]
     dp[1][ME] = dp[0][HIM] = 'W'
     dp[1][HIM] = dp[0][ME] = 'L'
@@ -16,6 +18,7 @@ def matches(n: int):
         if dp[i][0] == 'B':
             for j in range(i + i, count, i):
                 dp[j][ME] = dp[j][HIM] = ''
+    dp[n][ME] = dp[n][HIM] = ''
     for i in range(4, count):
         if dp[i][ME] == 'B':
             continue
@@ -27,12 +30,6 @@ def matches(n: int):
             l_count += dp[i - j][ME] == 'L'
         dp[i][ME] = 'W' if w_count else 'L'
         dp[i][HIM] = 'L' if l_count else 'W'
-    w_count, l_count = 0, 0
-    for j in range(1, 4):
-        if dp[n - j][ME] == 'B':
-            continue
-        w_count += dp[n - j][HIM] == 'W'
-    dp[n][ME] = 'W' if w_count else 'L'
     return 1 if dp[n][ME] == 'W' else 2
 
 
